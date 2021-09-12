de7dc3b96df4cfcd723bbb07b47dca6c5a8b0f4c
Mehdi <elwafi.courrier99@gmail.com>
2021-09-12 18:45:24 +0100

    Updated array.sh

--- array-1.sh
+++ array-1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-files=("f1.txt","f2.txt","f3.txt","f4.txt")
+files=("f1.txt" "f2.txt" "f3.txt" "f4.txt")  
+
+echo ${files[3]} ${files[2]} ${files[1]} ${files[0]} 
+
+echo ${files[*]}
