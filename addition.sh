87cb677e5c083b262d7fa92e84ad7feea32080d5
Mehdi <elwafi.courrier99@gmail.com>
2021-09-14 14:24:16 +0100

    Updated addition 2

--- addition.sh
+++ addition.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-fs1=${du -b $1 | cut -f1}
-fs2=${}
+fs1=$(du -b $1 | cut -f1)
+fs2=$(du -b $2 | cut -f1)
+
+echo "Size of $1 : $fs1"
+echo "Size of $2 : $fs2"
+
+result=$((fs1 + fs2))
+
+echo "Total size of $1 and $2: $result" 
