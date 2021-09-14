6a44638112f07841dd3eecddc790225b71188c34
Mehdi <elwafi.courrier99@gmail.com>
2021-09-14 02:12:07 +0100

    Added hybrid-array

---
+++ hybrid-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+user=("Dahbi" 748 "sysadmin sudo manager" "zsh")
+
+echo "Username : ${user[0]}"
+echo "UID : ${user[1]}"
+echo "User Groups : ${user[2]}"
+echo "Default Shell : ${user[3]}"
