dfd702d3330145c4e3adf957e699a86b857fb8b0
Mehdi <elwafi.courrier99@gmail.com>
2021-09-15 16:57:38 +0100

    Updated salary

--- salary.sh
+++ salary.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo "Entre monthly gross salary: "
+read sal
+echo "Entre tax percentage: "
+read per
+
+annual=$sal * 12
+annualtax=$(echo "scale=2; $annual * (1/$er)" | bc -l )
+
+echo "Your annual net profite is: $($annual - $annualtax)"
