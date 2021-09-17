9151dbee01ce59689238df23ba5148645e15cf94
Mehdi <elwafi.courrier99@gmail.com>
2021-09-17 13:51:25 +0100

    Updated age script

--- age.sh
+++ age.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+AGE=$1
+
+if [ $AGE -lt 13 ]; then
+	echo "Hello, Kid"
+elif [ $AGE -lt 20 ]; then
+	echo "Hello, teenager"
+elif [ $AGE -lt 60 ]; then
+	echo "Hello, Adult"
+else echo "Hello, elder" 
+fi	
