2171ee87fba2f82df152ffcefb3dd7fb59027192
Mehdi <elwafi.courrier99@gmail.com>
2021-09-18 13:48:51 +0100

    Added nested if con

---
+++ nested-if-con.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+TEMP=$1
+
+
+if [ $TEMP -gt 5  ]; then
+	if [ $TEMP -lt 15  ]; then
+		echo "The weather is cold."
+	elif [ $TEMP -lt 25  ]; then
+		echo "The weathe is cool"
+	else 
+		echo "The weather in hot."
+else
+	echo "Its Freezing outside"	
+fi
