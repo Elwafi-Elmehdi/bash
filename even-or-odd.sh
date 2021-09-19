e4dbd1bf83d487ef09852241c192a689a347198d
Mehdi <elwafi.courrier99@gmail.com>
2021-09-19 20:10:22 +0100

    Updated even or odd script 2

--- even-or-odd.sh
+++ even-or-odd.sh
@@ -2,3 +2,11 @@
 if [ $# -ne 1  ]; then
 	echo "invalid args number"
 fi
+
+if [ $1 % 2 -eq 0  ]; then
+	echo "$1 is even"
+elif [ $1 % 2 -eq 1 ]; then
+	echo "$1 is odd"
+else
+	echo "$1 is not an integer"
+fi
