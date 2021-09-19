b2859ccd3e4eacb5c436a4a56e0fda374b4ea1db
Mehdi <elwafi.courrier99@gmail.com>
2021-09-19 14:54:50 +0100

    Updated file.sh

--- file.sh
+++ file.sh
@@ -4,12 +4,12 @@
 file=$1
 
 if [ -e $file ]; then
-	if [ -d $file ]; then
-		echo "$file is a directory"
-	elif [ -f $file ]; then
+	if [ -f $file ]; then
 		echo "$file is a regular file"
-	elif [ -L $file  ]; then
+	elif [ -L $file ]; then
 		echo "$file is a soft link"
+	elif [ -d $file  ]; then
+		echo "$file is a directory"
 	fi
 else
 	echo "$file : does not exists on the fs"
