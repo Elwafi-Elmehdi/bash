1ce4cf9413a67faf6c8659b9d78fdd6269176480
Mehdi <elwafi.courrier99@gmail.com>
2021-09-19 15:13:43 +0100

    Updated leap year

--- leap-year.sh
+++ leap-year.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+if p [ $# -ne 1 ]; then
+	echo "Invalid number of args"
+	exit 1
+fi
 
+year=$1
+
+if [ $((year % 400)) -eq 0  ]; then 
+	echo "$year is a leap year"
+elif [ (  $((year % 4)) -eq 0 ) -o ( $((year %  100 )) -ne 0 ) ]; then
+	echo "$year is a leap year"
+else 
+	echo "$year is not leap year"
+fi
