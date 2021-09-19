9934d1ccb47daa8b6b38379b8613d6a9c958d382
Mehdi <elwafi.courrier99@gmail.com>
2021-09-19 14:39:50 +0100

    Updated switch

--- switch.sh
+++ switch.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
 
+
+c=$1
+
+case $c in
+	[a-z])
+		echo "Small Alphabet" ;;
+	[A-Z])
+		echo "Big Alphabet" ;;
+	[0-9])
+		echo "Number" ;;
+	*)
+		echo "Special Character" ;;
+
+esac
