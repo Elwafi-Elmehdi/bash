ae35cc0696335de7ede992807b3afc5d2db967ea
Mehdi <elwafi.courrier99@gmail.com>
2021-09-21 14:50:24 +0100

    Updated template.sh 2

--- template.sh
+++ template.sh
@@ -7,18 +7,16 @@ fi
 name=$1
 reason=$2
 
-prefix= $(cat <<EOF 
+cat <<EOF > $name 
 #!/usr/bin/env bash
 
 #REASON : `echo $reason`
 #Author : `echo $USER`
 #CREATED DATE : `date +"%F %T"`
 EOF 
-)
 
-echo $prefix > "$name.sh"
+#echo $prefix > "$name.sh"
 
 if [$? -eq 0];then
 	chmod u+x "$name.sh"
 fi
-
