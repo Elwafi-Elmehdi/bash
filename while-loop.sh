55fdb91e814bf9279244e165ae66c9267dfb3433
Mehdi <elwafi.courrier99@gmail.com>
2021-09-22 14:14:17 +0100

    Updated while script

--- while-loop.sh
+++ while-loop.sh
@@ -3,3 +3,11 @@
 #REASON : while loop with input field
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-22 00:40:32
+
+input="Hello"
+while [[ $input != "bye"  ]]; do
+	
+	echo "Entre a string value"
+	read input
+	echo "You entered $input"
+done
