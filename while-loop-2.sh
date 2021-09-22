609b52c7b4d131fe7787dbb16b28ba6c089a4d6f
Mehdi <elwafi.courrier99@gmail.com>
2021-09-22 18:00:05 +0100

    Updated while loop 2 script

--- while-loop-2.sh
+++ while-loop-2.sh
@@ -3,3 +3,10 @@
 #REASON : echo the multiplacation table of 3
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-22 17:58:12
+
+num=1
+while [ $num -le 10  ]; do
+
+	echo $(($num * 3))
+	num=$(($num + 1))
+done
