7b35046d47c0513dc46d82936aa944dbdb934b35
Mehdi <elwafi.courrier99@gmail.com>
2021-09-23 18:34:22 +0100

    Updated subnet script

--- subnet.sh
+++ subnet.sh
@@ -3,3 +3,12 @@
 #REASON : discover running server in sub net 1,255
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-23 18:29:11
+
+for ip in "192.168.1.{1,255}"; do
+	ping -C 2 $ip
+	if [ $? -eq 0  ]; then 
+		echo "$ip is up and running."
+	else
+		echo "$ip is unreachable"
+	fi 
+done
