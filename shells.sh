7a9ac8662aa3f9dc74f834528b2829f6e131091f
Mehdi <elwafi.courrier99@gmail.com>
2021-09-23 02:15:07 +0100

    Updated shells

--- shells.sh
+++ shells.sh
@@ -3,3 +3,7 @@
 #REASON : Display current shell and other shells
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-23 02:09:27
+
+echo "CURRENT SHELL $SHELL"
+echo "OTHER AVAILABLE SHELLS"
+awk -F'/' '{print $NF}' /etc/shells | grep -v '#'
