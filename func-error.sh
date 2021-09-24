54508c7a48df5a3916da541620a341a04431a24b
Mehdi <elwafi.courrier99@gmail.com>
2021-09-25 00:30:56 +0100

    Updated func-error

--- func-error.sh
+++ func-error.sh
@@ -3,3 +3,13 @@
 #REASON : return and echo the return val
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-25 00:29:02
+
+
+function error () {
+	babab
+	return 0
+}
+
+error
+
+echo "the return of the function is $?"
