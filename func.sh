bf42b47afb695192575052236dd0a444cde65ea9
Mehdi <elwafi.courrier99@gmail.com>
2021-09-24 22:55:59 +0100

    Updated func script

--- func.sh
+++ func.sh
@@ -3,3 +3,18 @@
 #REASON : my first function in bash
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-24 22:46:02
+
+hello () {
+	
+	if [[ -n $1  ]]; then
+		
+		echo "Hello, $1!"
+	else
+		echo "Hello, anonymous!"
+	fi
+	return 0
+}
+
+hello 
+
+hello "$USER"
