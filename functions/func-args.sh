b53f8fd37d87a0188747860e290ed01a2b745bc7
Mehdi <elwafi.courrier99@gmail.com>
2021-09-27 13:01:51 +0100

    Updated func-args

--- functions/func-args.sh
+++ functions/func-args.sh
@@ -3,3 +3,14 @@
 #REASON : create a func that display agrs passed to func
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-27 12:46:15
+
+func() {
+
+	echo "$1 is the first arg passed to function"
+	echo "$2 is the second arg passed to function"
+	printf "\n\n"
+}
+
+func 4 8
+func 147 578
+func "Mehdi ELwafi" "likes Bash"
