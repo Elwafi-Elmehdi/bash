827b2664a5e7b6bde189f614309f644de66ad56e
Mehdi <elwafi.courrier99@gmail.com>
2021-09-27 17:22:52 +0100

    Updated the global local func

--- functions/glob-local-vars.sh
+++ functions/glob-local-vars.sh
@@ -3,3 +3,18 @@
 #REASON : Exploring global and local vars
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-27 16:36:06
+
+v1='A'
+v2='B'
+
+myfunc () {
+	local v1='C'
+	v2='D'
+	echo "echo from inside myfunc : v1=$v1 , v2=$v2"
+}
+
+echo "echo from before calling myfunc : v1=$v1 , v2=$v2"
+
+myfunc
+
+echo "echo from after calling myfunc : v1=$v1 , v2=$v2"
