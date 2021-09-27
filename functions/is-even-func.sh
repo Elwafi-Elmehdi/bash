0f40b03dfbd93595527ecc5df97653b5a751ef76
Mehdi <elwafi.courrier99@gmail.com>
2021-09-27 12:43:37 +0100

    Updated is even function function

--- functions/is-even-func.sh
+++ functions/is-even-func.sh
@@ -3,3 +3,16 @@
 #REASON : tell if a number is even or not
 #AUTHOR : mehdi
 #CREATED DATE : 2021-09-27 12:38:04
+
+
+isEven() {
+	if [ $(( $1 % 2 )) -eq 0  ]; then
+		echo "Number : $1 is even"
+	else
+		echo "Number : $1 is odd"	
+	fi
+}
+
+isEven 4
+isEven 111
+isEven 47
