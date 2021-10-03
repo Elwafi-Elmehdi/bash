ee80ceb2746f9d68ee3e9058c63c0651c16f0fd4
Mehdi <elwafi.courrier99@gmail.com>
2021-10-03 17:24:09 +0100

    Updated recursion script 2

--- maths/recursion.sh
+++ maths/recursion.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-#REASON : infinite recursion function
+#REASON : infinite recursion function and using a exit condition
 #AUTHOR : mehdi
 #CREATED DATE : 2021-10-03 17:12:43
 
+counter=1
+
 function recur () {
 	echo "Hello Friend"
+	if [ $counter -gt 10  ]; then exit 0; fi
+       	counter=$(($counter + 1))	
 	recur
 }
 recur
