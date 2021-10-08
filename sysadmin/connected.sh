c33d114ade27fd6040e478149592bfb1fb8a07f9
Mehdi <elwafi.courrier99@gmail.com>
2021-10-08 14:23:59 +0100

    Updated connected script

--- sysadmin/connected.sh
+++ sysadmin/connected.sh
@@ -3,3 +3,11 @@
 #REASON : test internet connection
 #AUTHOR : mehdi
 #CREATED DATE : 2021-10-08 13:53:03
+
+ping -c  1 google.com 2> /dev/null
+
+if [ $? -eq 0  ]; then
+       	echo "You are connected!"
+else
+	echo "Failed to connect"
+fi	
