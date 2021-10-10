138a3f173807a51b034bf311fd81226e4fa68405
Mehdi <elwafi.courrier99@gmail.com>
2021-10-10 01:47:13 +0100

    Updated basic infos scrip

--- sysadmin/basic-infos.sh
+++ sysadmin/basic-infos.sh
@@ -3,3 +3,7 @@
 #REASON : print working dir hostname and dir contents
 #AUTHOR : mehdi
 #CREATED DATE : 2021-10-10 01:33:11
+
+echo "HOST	: $(hostname)"
+echo "PWD	: $(pwd)"
+echo "DATE	: $(date +%F-%T)"
